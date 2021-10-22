fe8e7a8b4b91fca619267448e5147ad487a70a46
ducnam0902 <ducnam0902@gmail.com>
2021-10-22 07:13:29 +0700

    addincresaga

---
+++ src/features/counter/counterSaga.ts
@@ -0,0 +1,17 @@
+import { delay, put, takeEvery } from "@redux-saga/core/effects";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { incrementSaga, incrementSagaSuccess } from "./counterSlice";
+// export function log(action: PayloadAction){
+//     console.log('Log', action);
+// }
+function* handleIncrementSaga(action: PayloadAction<number>){
+    console.log('Waiting 2s');
+    //Wait 2s
+    yield delay(2000);
+    console.log('Waiting action dispatch');
+    yield put(incrementSagaSuccess(action.payload));
+}
+export default function* counterSaga(){
+    console.log('Counter Saga');
+    yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
+}
